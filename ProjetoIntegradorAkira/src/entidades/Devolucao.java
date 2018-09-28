4068a32d0dc7b2faacfe761d7b9523072ee64778
Fernando <fernando_bento2@hotmail.com>
2018-09-27 21:32:13 -0300

    .

--- ProjetoIntegradorAkira/src/entidades/Devolucao.java
+++ ProjetoIntegradorAkira/src/entidades/Devolucao.java
@@ -45,6 +45,53 @@ public class Devolucao {
 	public void setEstaDanificado(boolean estaDanificado) {
 		this.estaDanificado = estaDanificado;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dtRealDevolucao == null) ? 0 : dtRealDevolucao.hashCode());
+		result = prime * result + (estaDanificado ? 1231 : 1237);
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((locacao == null) ? 0 : locacao.hashCode());
+		result = prime * result + (statusPagamento ? 1231 : 1237);
+		result = prime * result + ((valorTotal == null) ? 0 : valorTotal.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Devolucao other = (Devolucao) obj;
+		if (dtRealDevolucao == null) {
+			if (other.dtRealDevolucao != null)
+				return false;
+		} else if (!dtRealDevolucao.equals(other.dtRealDevolucao))
+			return false;
+		if (estaDanificado != other.estaDanificado)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (locacao == null) {
+			if (other.locacao != null)
+				return false;
+		} else if (!locacao.equals(other.locacao))
+			return false;
+		if (statusPagamento != other.statusPagamento)
+			return false;
+		if (valorTotal == null) {
+			if (other.valorTotal != null)
+				return false;
+		} else if (!valorTotal.equals(other.valorTotal))
+			return false;
+		return true;
+	}
 	
 	
 }
